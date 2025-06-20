512b051219fff74cdab5b2bcd81e8aefed1c6a76
elect0 <educinca8@gmail.com>
2025-06-20 05:32:06 -0700

    feat(booking): implement interactive availability UI with shadcn

--- ora-fixa/src/lib/components/landing/booking.svelte
+++ ora-fixa/src/lib/components/landing/booking.svelte
@@ -25,16 +25,16 @@
 		selectedServiceId ? services.find((s: Service) => s.id.toString() === selectedServiceId) : null
 	);
 
+	interface Slot {
+		available_slot: string;
+	}
+
 	$effect(() => {
 		if (selectedDate && selectedService) {
 			fetchAvailableTimes(selectedDate);
 			console.log('FETCH');
 		}
 	});
-
-	function selectService(service: Service) {
-		selectedService = service;
-	}
 
 	async function fetchAvailableTimes(date: DateValue) {
 		if (!selectedService) return;
@@ -49,7 +49,8 @@
 
 			if (response.ok) {
 				const data = await response.json();
-				availableSlots = data.availableSlots || [];
+				console.log(data.slots);
+				availableSlots = data.slots || [];
 			}
 		} catch (error) {
 			console.error('A apărut o eroare în funcția fetch:', error);
@@ -64,10 +65,11 @@
 	});
 
 	let selectedDate: DateValue | undefined = $state(undefined);
-	let selectedTime: string | undefined = $state(undefined);
+	let selectedTime: string | '' = $state('');
 
-	let availableSlots: string[] = $state([]);
+	let availableSlots: Slot[] = $state([]);
 	let isLoading = $state(false);
+	console.log(selectedTime);
 </script>
 
 <section id="booking" class="bg-gradient-to-br from-amber-50 to-stone-100 py-24">
@@ -149,21 +151,86 @@
 							{#if selectedService}
 								<AccordionContent>
 									<div class="border-border-stone-200 flex justify-center rounded-xl bg-white p-4">
-										{#if selectedService}
-											<div>
-												<Calendar
-													bind:value={selectedDate}
-													type="single"
-													disableDaysOutsideMonth={true}
-												/>
-												<p class="mt-4 text-center text-sm text-stone-600">
-													Dată selectată: <span class="font-medium"
-														>{selectedDate
-															? df.format(selectedDate.toDate(getLocalTimeZone()))
-															: 'Alege o dată'}</span
-													>
-												</p>
-											</div>
+										<div>
+											<Calendar
+												bind:value={selectedDate}
+												type="single"
+												disableDaysOutsideMonth={true}
+											/>
+											<p class="mt-4 text-center text-sm text-stone-600">
+												Dată selectată: <span class="font-medium"
+													>{selectedDate
+														? df.format(selectedDate.toDate(getLocalTimeZone()))
+														: 'Alege o dată'}</span
+												>
+											</p>
+										</div>
+									</div>
+								</AccordionContent>
+							{/if}
+						</AccordionItem>
+						<AccordionItem
+							value="item-3"
+							class="space-y-4"
+							disabled={!selectedDate || !selectedService}
+						>
+							<AccordionTrigger
+								class="mb-4 flex cursor-pointer items-center gap-3 no-underline hover:no-underline focus:no-underline"
+							>
+								<div
+									class="flex h-8 w-8 items-center justify-center rounded-full font-bold"
+									class:bg-amber-600={selectedService && selectedDate}
+									class:text-white={selectedService && selectedDate}
+									class:bg-stone-300={!selectedService || !selectedDate}
+									class:text-stone-600={!selectedService || !selectedDate}
+								>
+									3
+								</div>
+								<h3
+									class="text-xl font-semibold"
+									class:text-stone-900={selectedService && selectedDate}
+									class:text-stone-600={!selectedService || !selectedDate}
+								>
+									{selectedService && selectedDate
+										? 'Alege Ora'
+										: 'Alege data (dupa selectarea serviciului si/sau a orei)'}
+								</h3>
+							</AccordionTrigger>
+							{#if selectedDate && selectedService}
+								<AccordionContent class="pt-4">
+									<div class="min-h-[8rem] rounded-lg bg-stone-50 p-4">
+										{#if isLoading}
+											<p class="animate-pulse text-center text-stone-500">Se încarcă orele...</p>
+										{:else if availableSlots.length > 0}
+											<RadioGroup.Root
+												bind:value={selectedTime}
+												class="grid grid-cols-3 gap-3 md:grid-cols-5"
+											>
+												{#each availableSlots as slot (slot)}
+													<div>
+														<RadioGroup.Item
+															value={slot.available_slot}
+															id={`time-${slot.available_slot}`}
+															class="peer sr-only"
+														/>
+														<Label
+															for={`time-${slot.available_slot}`}
+															class="w-full cursor-pointer rounded-md border-2 border-stone-200 bg-white p-3 text-center text-sm font-semibold transition-all hover:bg-stone-100
+										peer-data-[state=checked]:border-amber-600
+										peer-data-[state=checked]:bg-amber-600
+										peer-data-[state=checked]:text-white
+										peer-data-[state=checked]:shadow-md
+										"
+														>
+															{slot.available_slot}
+														</Label>
+													</div>
+												{/each}
+											</RadioGroup.Root>
+										{:else}
+											<p class="text-center font-medium text-red-600">
+												Nu sunt ore disponibile în ziua selectată.
+											</p>
 										{/if}
 									</div>
 								</AccordionContent>
