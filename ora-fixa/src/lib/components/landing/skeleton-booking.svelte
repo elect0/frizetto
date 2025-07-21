9676486d53ae228dbd497d70321bcde743f4dc46
elect0 <96078234+elect0@users.noreply.github.com>
2025-07-21 17:47:52 +0300

    fix(performance): Eliminate CLS with a high-fidelity skeleton loader (#3)

---
+++ ora-fixa/src/lib/components/landing/skeleton-booking.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
+</script>
+
+<section id="booking" class="py-24 text-center">
+	<div class="container mx-auto px-4 lg:px-6">
+		<div class="mx-auto max-w-4xl">
+			<div class="mb-12 flex flex-col items-center space-y-8">
+				<Skeleton class="h-8 w-32 md:h-9 md:w-36" />
+				<Skeleton class="md:w-140 w-85 h-9 md:h-12" />
+				<Skeleton class="md:w-160 w-90 h-20 md:h-7" />
+				<Skeleton class="md:w-224 w-90 h-150 md:h-100" />
+			</div>
+		</div>
+	</div>
+</section>
