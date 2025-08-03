0646f0316473101535c899c78e18da1d86552b1f
elect0 <educinca8@gmail.com>
2025-08-03 10:11:46 +0300

    feat(dashboard): implement service management page

---
+++ ora-fixa/src/routes/(admin)/admin/servicii/+page.server.ts
@@ -0,0 +1,72 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
+import { fail, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { addServiceSchema, updateServiceSchema } from '$lib/schemas';
+
+export const load: PageServerLoad = async ({ locals: { supabase, session } }) => {
+	const addServiceForm = await superValidate(zod(addServiceSchema));
+
+	const { data: services, error: servicesError } = await supabase.rpc('get_service_popularity');
+
+	if (servicesError) {
+		console.error(servicesError);
+		throw error(500, 'A apărut o eroare la server.');
+	}
+
+	return {
+		services,
+		addServiceForm,
+		session
+	};
+};
+
+export const actions: Actions = {
+	updateService: async ({ request, locals: { supabase } }) => {
+		const form = await superValidate(request, zod(updateServiceSchema));
+
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		const { error: serviceError } = await supabase
+			.from('services')
+			.update({
+				name: form.data.name,
+				description: form.data.description,
+				price: form.data.price,
+				duration_minutes: form.data.duration
+			})
+			.eq('id', parseInt(form.data.serviceId));
+
+		if (serviceError) {
+			return fail(400, { message: 'A apărut o eroare la server.' });
+		}
+
+		return { form };
+	},
+
+	addService: async ({ request, locals: { supabase } }) => {
+		const form = await superValidate(request, zod(addServiceSchema));
+
+		console.log(form.data)
+
+
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		const { error: serviceError } = await supabase.from('services').insert({
+			name: form.data.name,
+			description: form.data.description,
+			price: form.data.price,
+			duration_minutes: form.data.duration
+		});
+
+		if (serviceError) {
+			return fail(400, { message: 'A apărut o eroare la server.' });
+		}
+
+		return { form };
+	}
+};
---
+++ ora-fixa/src/routes/(admin)/admin/servicii/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import AddServiceModal from "$lib/components/add-service-modal.svelte";
+	import ServiceCard from "$lib/components/service-card.svelte";
+    let {data} = $props()
+</script>
+
+<div class="flex flex-1 flex-col">
+	<div class="@container/main flex flex-1 flex-col gap-2">
+		<div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+			<div class="px-4 lg:px-6">
+                <div class="flex justify-between">
+                    <h1 class="mb-6 text-3xl font-medium">Management</h1>
+                    <AddServiceModal addServiceForm={data.addServiceForm} />
+                </div>
+                                <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+                                    {#each data.services as service (service.service_id) }
+                                        <ServiceCard {service} /> 
+                                    {/each}
+                                </div>
+            </div>
+		</div>
+	</div>
+</div>
+
