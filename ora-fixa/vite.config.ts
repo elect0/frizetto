17efb837cb20ee55dbebf27c15556d5980027857
elect0 <educinca8@gmail.com>
2025-10-29 07:15:04 +0200

    feat: implement pwa

--- ora-fixa/vite.config.ts
+++ ora-fixa/vite.config.ts
@@ -2,10 +2,64 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import { imagetools } from "vite-imagetools";
+import { imagetools } from 'vite-imagetools';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [enhancedImages(), imagetools(),tailwindcss(), sveltekit()],
+	plugins: [
+		enhancedImages(),
+		imagetools({
+			defaultDirectives: () =>
+				new URLSearchParams({
+					format: 'webp'
+				})
+		}),
+		tailwindcss(),
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			},
+			strategies: 'generateSW',
+			manifest: {
+				name: 'Frizetto',
+				short_name: 'Frizetto',
+				description: 'Test',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				display: 'standalone',
+				scope: '/',
+				start_url: '/',
+				icons: [
+					{
+						src: '/192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/256.png',
+						sizes: '256x256',
+						type: 'image/png'
+					},
+					{
+						src: '/512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			},
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,svg,webp,avif}'],
+				
+        	globIgnores: ['**/*.png'],
+        maximumFileSizeToCacheInBytes: 2097152
+			}
+		})
+	],
+	build: {
+		assetsInlineLimit: 4096
+	},
 	ssr: {
 		noExternal: ['layerchart'] // this is the important part
 	}
