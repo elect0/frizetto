c382efa5dd0e3373af7cbfb08b255cb8204001b1
elect0 <educinca8@gmail.com>
2025-11-05 16:25:52 +0200

    fix

--- ora-fixa/src/routes/+layout.svelte
+++ ora-fixa/src/routes/+layout.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 	import { pwaInfo } from 'virtual:pwa-info';
-	import { dev } from '$app/environment';
 
 	let { children } = $props();
 
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 
-	navigator.serviceWorker.register('/service-worker.js', {
-		type: dev ? 'module' : 'classic'
-	});
 
 	let webManifestLink = $derived(pwaInfo ? pwaInfo.webManifest.linkTag : '');
 </script>
